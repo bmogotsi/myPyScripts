1921add73f45558ea927dce62640a3889a6f0b01
“Ben <Ben.Mogotsi@gmail.com>
2025-08-19 00:14:10 +0200

    search_string_in_file_full_read.txt

--- py_explorer/dir_readAllFilesInFolder.py
+++ py_explorer/dir_readAllFilesInFolder.py
@@ -7,12 +7,15 @@ from pathlib import Path
 import datetime
 from datetime import date
 import re
+import csv
 
 
 # import date
 # path_source = 'C:/Users/Ben.Mogotsi/myPyScripts/'
+cwd = cwd = Path.cwd()
 path_target = 'C:/Users/Ben.Mogotsi/myPyScripts/'
-path_source=r'C:\Users\Ben.Mogotsi\OneDrive - Momentum Group\Documents\My Documents\Liscoe\SquirreL_myFiles'
+# path_source=r'C:\Users\Ben.Mogotsi\OneDrive - Momentum Group\Documents\My Documents\Liscoe\SquirreL_myFiles'
+path_source = cwd
 
 try:
     def search_string_in_file_full_read(file_path, search_string):
@@ -31,6 +34,7 @@ try:
                 content = file.read()
                 search_string.find(search_string) != -1
                 if re.search(search_string, content, re.IGNORECASE):
+                        return True
                         print(f"Found (case-insensitive) '{search_string}' in file '{file_path}' using regex")
                 return search_string.casefold() in content.casefold()
                 # The casefold() method converts a string into a casefolded form,
@@ -42,17 +46,122 @@ try:
         except FileNotFoundError:
             print(f"Error: File not found at {file_path}")
             return False
+            
+    def create_file(fileName, suffix):
+        """
+            Create an empty file name
+                add blank lines before
+        """
+        if suffix == "":
+            suffix = ".txt"
+        if suffix[0] != ".":
+            suffix = "." + suffix
+            
+        # create file if it does not exist       
+        # if not Path(fileName+suffix).exists():
+        if suffix == ".txt":
+            file=open(fileName+suffix, mode="w",encoding="utf-8") # clears file if it exists
+            file.close
+        elif suffix == ".csv":
+            new_headings = [["Full Path Name", "Search String", "Found"]]
+            with open(fileName+suffix, "w", newline='') as file:
+                writer = csv.writer(file, quoting=csv.QUOTE_NONE,
+                                    delimiter=',', quotechar='"', escapechar='\\')
+                writer.writerows(new_headings)
+        
+    def open_file(fileName, suffix):
+        """
+            Open file in append mode
+            create file if it does not exist   
+                    
+        """
+        # create file if it does not exist   
+        # append file
+        
+        if suffix == "":
+            suffix = ".txt"
+        if suffix[0] != ".":
+            suffix = "." + suffix
+        # Check if the file exists, if not create it
+        # return file object
+        if suffix == ".txt":
+            file_obj = open(fileName+suffix, mode="a",encoding="utf-8")
+            return file_obj, fileName+suffix
+        elif suffix == ".csv":
+            new_headings = ['Full Path Name', 'Search String', 'Found']
+            with open(fileName+suffix, 'a', newline='') as file:
+                writer = csv.writer(file, quoting=csv.QUOTE_NONE,
+                                    delimiter=',', quotechar='"', escapechar='\\')
+            return writer, fileName+suffix
 
-    string_to_find = "with "
+        
+    def close_file(fileObj):
+        """
+            Close file 
+                        
+        """
+        if fileObj is not None:
+            fileObj.close()
+            
+    def is_csv_writer(obj):
+        """
+        Checks if an object behaves like a csv.writer by checking for the 'writerow' method.
+        """
+        return hasattr(obj, 'writerow') and callable(getattr(obj, 'writerow'))  
+        
+    def write_to_file(fileobj, filename, new_line, blank_lines=1):
+        """
+            Write text string to text file
+                add blank lines before
+                new_line: text to write to file
+                blank_lines: number of blank lines to add before writing the new line
+                
+        """
+        
+        if fileobj is not None:  
+            if is_csv_writer(fileobj):
+                # If fileobj is a CSV writer, write a row
+                # fileobj.writerow(new_line)  # Write the new line
+                with open(filename, 'a', newline='') as file:
+                    # writer = csv.writer(file,  
+                    #                     delimiter=',', quotechar='"', escapechar='\\') # quoting=csv.QUOTE_NONE "not required for blank lines"
+                    #                     #
+                    # # Write blank lines before writing the new line
+                    # writer.writerow([''] * blank_lines)  # Write blank lines
+                    writer = csv.writer(file, quoting=csv.QUOTE_NONE,
+                                        delimiter=',', quotechar='"', escapechar='\\')
+                    writer.writerow(new_line)  # Write the new line
+            else:
+                # Otherwise, assume it's a regular file object and write a string
+                new_line = str(new_line)
+                # Write blank lines before writing the new line
+                fileobj.write("\n" * blank_lines)
+                fileobj.write(f"{new_line}")
+            return True
+        else:
+            return False
+    
+
+    file_name = "search_string_in_file_full_read"
+    create_file(file_name, ".txt")
+    create_file(file_name, ".csv")
+    
+    fileobj_txt, file_txt_name = open_file(file_name, ".txt")
+    fileobj_csv, file_csv_name = open_file(file_name, ".csv")
+    
+    string_to_find = "with "    
     for file in Path(path_source).glob('*'):
         if file.is_file():
-            print(f"stem:  {file.name} or {file.stem}  or {file.suffix} ")
-            file_name = file.name
-            if search_string_in_file_full_read(file_name, string_to_find):
-                print(f"'{string_to_find}' found in {file_name}")
-            else:
-                print(f"'{string_to_find}' not found in {file_name}")
-        else:
+            print(f" {file.name} ")
+            full_path = Path(path_source) / Path(file.name) 
+            if search_string_in_file_full_read(full_path, string_to_find):
+                write_to_file(fileobj_txt, file_txt_name, f"'{string_to_find}' found in {full_path}")
+                write_to_file(fileobj_csv, file_csv_name, [full_path, string_to_find, 'Yes'])
+                continue
+                print(f"'{string_to_find}' found in {full_path}")
+            elif False:
+                print(f"'{string_to_find}' not found in {full_path}")
+        elif file.is_file == True:
             print(f" path: {file}  st_size: {file.stat().st_size/1000}   suffix: {file.suffix} \
             st_ctime  {datetime.datetime.fromtimestamp(file.stat().st_ctime)} \
              st_atime: {datetime.datetime.fromtimestamp(file.stat().st_atime)}  \
@@ -69,10 +178,12 @@ try:
 
 except Exception as e:
     print(f"Error: {str(e)}")
-
-# Example usage:
-
-
-
-
+    
+finally:
+    if 'fileobj_txt' in locals() and not fileobj_txt.closed: # Check if file object exists and is open
+        close_file(fileobj_txt)
+    #  if 'fileobj_csv' in locals() and not fileobj_csv.closed: # Check if file object exists and is open
+    #      close_file(fileobj_csv)
+    print("File closed successfully.")
+    
 quit()
